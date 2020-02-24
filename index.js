acbacbb6aff972a4ff44c1ec110fb1fdcd384635
Saurav Pathak <saurab.pathak.0@gmail.com>
2020-02-24 22:19:41 +0545

    gameOver logic

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ const WIDTH = canvas.width;
 const GRAVITY = 1; 
 var count = 1;
 var score = 0;
+gameStop = false;
 
 
 function Bird(size){
@@ -47,17 +48,19 @@ function Bird(size){
 function Wall(height, size, turn, gap, distance){
     this.x = WIDTH + turn*distance*100
     this.size = size
+    this.gap = gap
+    this.height = height
 
-    this.update = function(){
-        this.x -=2;        
-        this.drawWall();
-    }
-
-    this.drawWall = function(){        
-        if(false)
+    this.update = function(isActive){
+        this.x -=2;                
+        if(isActive)
             ctx.fillStyle = "orange";
         else
             ctx.fillStyle = "green";
+        this.drawWall();
+    }
+
+    this.drawWall = function(){                
         ctx.fillRect(this.x,0, size, height);
         ctx.fillRect(this.x-3,height-4, size+6, 4);
 
@@ -72,10 +75,11 @@ function Wall(height, size, turn, gap, distance){
 function setup(){   
     count = 0
     score = 0 
+    gameStop = false
     bird = new Bird(20)
     walls = []
     for(i=0;i<5;i++){
-        walls[i] = new Wall(40, 35, i, 40, 0.8)
+        walls[i] = new Wall(40, 35, i, 40, 1)
     }
         
 }
@@ -83,26 +87,31 @@ function setup(){
 function startGame(){    
 
     bird.update(count)
-    
-    for(i=0;i<walls.length;i++){
-            walls[i].update(isWallActive(bird,walls[i]))
-        // walls.updateElem(i, changing)
-    }    
 
-    if(walls[0].x< -walls[0].size){
+    if(walls[0].x < - walls[0].size - 20 ){
         score++;
         walls = walls.slice(1, )
         wall = new Wall(40, 35, 1, 50, 0.9)
         walls.push(wall)
     }
+    
+    for(i=0;i<walls.length;i++){
+        if(isWallActive(bird, walls[i]))
+            walls[i].update(true)
+        else
+            walls[i].update(false)
+    }    
+    
 
     if(gameOver(bird, walls[0])){
         const gameOverMusic = document.getElementById("gameOver");
         gameOverMusic.play()
+        gameStop = true
         return
     }        
     count+=0.05;
     window.requestAnimationFrame(startGame);
+    console.log("animating")
 }
 
 function gameOver(bird, wall){
@@ -118,31 +127,32 @@ function gameOver(bird, wall){
 }
 
 function collide(bird, wall){
-    // if(bird.x==wall.x && bird.y>height+gap ){
-        // document.write("touched")
-    // }
-    // else
+    if(isWallActive(bird, wall) && ((bird.y <= wall.height) || bird.y+bird.size >= (wall.height+wall.gap))){
+        return true
+    }
+    else
         return false
 }
 
 function isWallActive(bird, wall){
-    return false
-
+    if(wall.x <= 40 && wall.x>=-wall.size){
+        return true
+    }        
+    else{
+        return false
+    }        
 }
-
 
 
 //actual events happening
 setup();
 var request = window.requestAnimationFrame(startGame);
 window.addEventListener("keypress", function(e){
-    if(!gameOver(bird, walls)){
-        bird.update(0, true);  
+    if(!gameStop){
+        bird.update(0, true);
         count = 0
-        
     }
     else{
-        document.write(score)
         setup();
         var request = window.requestAnimationFrame(startGame);        
     }
